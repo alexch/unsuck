7a01b538304c8a413fcc8294aec7810892fdd7ff
Alex Chaffee <alex@stinky.com>
2013-05-20 08:15:38 -0700

    a few more path, rect and ellipse methods

--- unsuck/CGCanvas+Extras.h
+++ unsuck/CGCanvas+Extras.h
@@ -13,19 +13,16 @@
 
 -(void)drawLineFromPoint:(CGPoint)fromPoint toPoint:(CGPoint)toPoint;
 
-// note: some of these aren't implemented yet
-
 -(id)fillRect:(CGRect)rect withColor:(UIColor*)color;
--(id)fillRect:(CGRect)rect withColor:(UIColor*)color andLineWidth:(CGFloat)lineWidth;
 -(id)strokeRect:(CGRect)rect withColor:(UIColor*)color;
 -(id)strokeRect:(CGRect)rect withColor:(UIColor*)color andLineWidth:(CGFloat)lineWidth;
 
 -(id)fillEllipse:(CGRect)rect withColor:(UIColor*)color;
--(id)fillEllipse:(CGRect)rect withColor:(UIColor*)color andLineWidth:(CGFloat)lineWidth;
 -(id)strokeEllipse:(CGRect)rect withColor:(UIColor*)color;
 -(id)strokeEllipse:(CGRect)rect withColor:(UIColor*)color andLineWidth:(CGFloat)lineWidth;
 
 -(void)fillPath:(UIBezierPath*)path;
+-(void)fillPath:(UIBezierPath*)path  withColor:(UIColor*);
 -(void)strokePath:(UIBezierPath*)path;
 -(void)strokePath:(UIBezierPath*)path withColor:(UIColor*)color andLineWidth:(CGFloat)lineWidth;
 
